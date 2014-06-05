1456aca1edcc3982eb8eb212daf9dd69dd4f440f
Nik Kolev <nikola_kolev@cable.comcast.com>
2014-06-05 13:43:01 -0400

    Adds date marshalling. Configures JSON marshalling of dates to be consistent with the XML one.

--- src/main/java/io/github/gv0tch0/sotaro/Say.java
+++ src/main/java/io/github/gv0tch0/sotaro/Say.java
@@ -1,6 +1,12 @@
 package io.github.gv0tch0.sotaro;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 import javax.xml.bind.JAXBElement;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -12,6 +18,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Say {
   private final static ObjectFactory JAXB_FACTORY = new ObjectFactory();
+  private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+  private final static DatatypeFactory CALENDAR_FACTORY;
+  static {
+    try {
+      CALENDAR_FACTORY = DatatypeFactory.newInstance();
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
 
   @RequestMapping(value = "/say/{what}", 
                   produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, 
@@ -23,6 +38,15 @@ public class Say {
   private JAXBElement<SayWhat> echo(String what) {
     SayWhat echo = new SayWhat();
     echo.setWhat(what);
+    echo.setWhen(utcNow());
     return JAXB_FACTORY.createSay(echo);
+  }
+
+  private XMLGregorianCalendar utcNow() {
+    GregorianCalendar c = new GregorianCalendar(UTC);
+    c.setTime(new Date());
+    XMLGregorianCalendar now = CALENDAR_FACTORY.newXMLGregorianCalendar(c);
+    now.setFractionalSecond(null);
+    return now;
   }
 }
